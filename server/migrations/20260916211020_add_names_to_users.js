d718cd97c83b727cd5e66b87622faf493941bece
luisfelipemontoya <luisfelipemontoya88@gmail.com>
2026-09-17 15:48:33 -0500

    Add user name fields

---
+++ server/migrations/20260916211020_add_names_to_users.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+/*
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = (knex) =>
+    knex.schema.table('users', (table) => {
+        table.string('first_name').notNullable().defaultTo('');
+        table.string('last_name').notNullable().defaultTo('');
+    });
+
+/*
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = (knex) =>
+    knex.schema.table('users', (table) => {
+    table.dropColumn('first_name');
+    table.dropColumn('last_name');
+  });
+
